668c82d48faa77019830e8f753de55f8349e3461
abarnatan <ale.barn19@gmail.com>
2021-02-04 20:47:22 -0800

    got markers working with large data set

--- ADB/PythonCode.py
+++ ADB/PythonCode.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 # read in CSV data file
-dataanalyst = pd.read_csv("./DataAnalystSMALL.csv", delimiter=",")
+dataanalyst = pd.read_csv("./DataAnalystFULL.csv", delimiter=",")
 # write to JSON file
-with open("DataAnalystSMALL.json","w") as file:
+with open("DataAnalystFULL.json","w") as file:
     file.write(dataanalyst.to_json())
