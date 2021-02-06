ee6b646c1aaf962d3861d98d0bf0a6560bbece31
schoo424 <Sung.Choo@nike.com>
2021-02-06 14:35:58 -0800

    updated csv

--- Choo/PythonCode.py
+++ Choo/PythonCode.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 # read in CSV data file
-dataanalyst = pd.read_csv("./DataAnalystDropJobDescription.csv", delimiter=",")
+dataanalyst = pd.read_csv("./DataAnalstScrubbed.csv", delimiter=",")
 # write to JSON file
-with open("DataAnalystDropJobDescription.json","w") as file:
+with open("DataAnalstScrubbed.json","w") as file:
     file.write(dataanalyst.to_json())
