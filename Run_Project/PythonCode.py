5ba19354fb9b9e3a9b60a3232397b279fa1a9e7f
abarnatan <ale.barn19@gmail.com>
2021-02-10 20:49:56 -0800

    cleaned up final folder

--- Run_Project/PythonCode.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import os
-import numpy as np
-# read in CSV data file
-dataanalyst = pd.read_csv("./AverageSalaryData.csv", delimiter=",")
-# write to JSON file
-with open("AverageSalaryData.json","w") as file:
-    file.write(dataanalyst.to_json())
